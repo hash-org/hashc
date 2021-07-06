1a0221db149ee2bc17f7a9b7f59d7c22765cf3fc
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2021-07-06 21:42:02 +0100

    Added a compiler panic handler with formatting and backtracing.

--- compiler/hash/src/main.rs
+++ compiler/hash/src/main.rs
@@ -2,14 +2,17 @@
 //
 // All rights reserved 2021 (c) The Hash Language authors
 
+use backtrace::Backtrace;
 use clap::{crate_version, AppSettings, Clap};
 use hash_ast::parse::{Parser, SeqParser};
 use hash_interactive::init;
 use hash_pest_parser::grammar::HashGrammar;
 use hash_reporting::errors::CompilerError;
 use log::log_enabled;
+use std::panic;
 use std::{
     env, fs,
+    panic::PanicInfo,
     time::{Duration, Instant},
 };
 
@@ -74,7 +77,43 @@ struct IrGen {
     debug: bool,
 }
 
+fn panic_handler(info: &PanicInfo) {
+    if let Some(s) = info.payload().downcast_ref::<&str>() {
+        println!("Sorry :^(\nInternal Panic: {}\n", s);
+    } else {
+        println!("Sorry :^(\nInternal Panic\n");
+    }
+
+    // Display the location if we can...
+    if let Some(location) = info.location() {
+        println!(
+            "Occurred in file '{}' at {}:{}",
+            location.file(),
+            location.line(),
+            location.column()
+        );
+    }
+
+    // print the backtrace
+    println!("Backtrace:\n{:?}", Backtrace::new());
+
+    let msg = "This is an interpreter bug, please file a bug report at";
+    let uri = "https://github.com/hash-org/lang/issues";
+
+    println!("{}\n\n{:^len$}\n", msg, uri, len = msg.len());
+}
+
 fn main() {
+    // Setup our panic handler
+    panic::set_hook(Box::new(panic_handler));
+
+    fn execute(f: impl FnOnce() -> Result<(), CompilerError>) {
+        match f() {
+            Ok(()) => (),
+            Err(e) => e.report_and_exit(),
+        }
+    }
+
     execute(|| {
         pretty_env_logger::init();
 
@@ -88,7 +127,6 @@ fn main() {
                 println!("Running with {}", path);
             }
         }
-
         // check here if we are operating in a special mode...
         if let Some(mode) = opts.mode {
             match mode {
@@ -109,8 +147,6 @@ fn main() {
                 let parser = SeqParser::new(HashGrammar);
                 let directory = env::current_dir().unwrap();
 
-                // @@TODO: this should be a compiler error instead of a ParseError, let's unify errors so that everyone uses CompilerError?
-                //         We could also move all the error stuff into hash_error
                 let result = timed(
                     || parser.parse(&filename, &directory),
                     log::Level::Debug,
@@ -129,13 +165,6 @@ fn main() {
             }
         }
     })
-}
-
-fn execute(f: impl FnOnce() -> Result<(), CompilerError>) {
-    match f() {
-        Ok(()) => (),
-        Err(e) => e.report_and_exit(),
-    }
 }
 
 #[inline(always)]
