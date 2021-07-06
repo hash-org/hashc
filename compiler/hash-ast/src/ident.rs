23d502ae342d72af13913c7090dd55b3ca45dd15
Constantine Theocharis <cthe@mailbox.org>
2021-07-06 21:35:31 +0000

    Compute reverse identifier mapping on-demand rather than in the end.
    This simplifies state management and avoids a lot of global variable
    headaches.
    
    Also, fix formatting.

--- compiler/hash-ast/src/ident.rs
+++ compiler/hash-ast/src/ident.rs
@@ -2,7 +2,6 @@ use crate::ast::AstString;
 use dashmap::DashMap;
 use hash_utils::counter;
 use lazy_static::lazy_static;
-use std::sync::atomic::{AtomicBool, Ordering};
 
 counter! {
     name: Identifier,
@@ -24,14 +23,13 @@ pub struct IdentifierMap {
     path_ident_data: DashMap<AstString, PathIdentifier>,
     path_ident_data_rev: DashMap<PathIdentifier, AstString>,
     ident_data_rev: DashMap<Identifier, AstString>,
-    revs_computed: AtomicBool,
 }
 
 lazy_static! {
     pub static ref IDENTIFIER_MAP: IdentifierMap = IdentifierMap::default();
 }
 
-pub struct PathIdentifierName(String);
+pub struct PathIdentifierName(AstString);
 
 impl PathIdentifierName {
     pub fn full(&self) -> &str {
@@ -49,7 +47,9 @@ impl IdentifierMap {
             *key
         } else {
             let ident = Identifier::new();
+            let ident_str_cpy = ident_str.clone();
             self.ident_data.insert(ident_str, ident);
+            self.ident_data_rev.insert(ident, ident_str_cpy);
             ident
         }
     }
@@ -59,42 +59,25 @@ impl IdentifierMap {
             *key
         } else {
             let path_ident = PathIdentifier::new();
+            let path_ident_str_cpy = path_ident_str.clone();
             self.path_ident_data.insert(path_ident_str, path_ident);
+            self.path_ident_data_rev
+                .insert(path_ident, path_ident_str_cpy);
             path_ident
         }
     }
 
-    pub fn compute_reverses(&self) {
-        for entry in self.ident_data.iter() {
-            self.ident_data_rev
-                .insert(entry.value().to_owned(), entry.key().to_owned());
-        }
-
-        for entry in self.path_ident_data.iter() {
-            self.path_ident_data_rev
-                .insert(entry.value().to_owned(), entry.key().to_owned());
-        }
-    }
-
-    fn ensure_reverses_computed(&self) {
-        if !self.revs_computed.load(Ordering::SeqCst) {
-            panic!("Tried to access reverses but they have not been computed")
-        }
-    }
-
     pub fn ident_name(&self, ident: Identifier) -> String {
-        self.ensure_reverses_computed();
         self.ident_data_rev.get(&ident).unwrap().value().to_string()
     }
 
     pub fn path_ident_name(&self, path_ident: PathIdentifier) -> PathIdentifierName {
-        self.ensure_reverses_computed();
         PathIdentifierName(
             self.path_ident_data_rev
                 .get(&path_ident)
                 .unwrap()
                 .value()
-                .to_string(),
+                .clone(),
         )
     }
 }
