21100b9463770230c0cc424790e0b8e07ac2a87b
Constantine Theocharis <cthe@mailbox.org>
2021-07-09 22:15:42 +0000

    Fix cargo errors

--- compiler/hash-utils/src/testing.rs
+++ compiler/hash-utils/src/testing.rs
@@ -1,16 +1,5 @@
 use std::path::PathBuf;
 
-// pub struct TestingSuccess {
-//     visualiser_output: String,
-//     ast_debug_output: String,
-// }
-
-// pub struct TestingFailure {
-//     error_debug_output: String,
-// }
-
-// pub type TestingResult = Result<TestingSuccess, TestingFailure>;
-
 #[derive(Debug, Clone)]
 pub struct TestingInput {
     pub path: PathBuf,
