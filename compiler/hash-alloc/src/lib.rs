7888d49e99d801a58ae00453935ac21b702cecdb
Constantine Theocharis <cthe@mailbox.org>
2021-07-22 19:55:15 +0000

    Alloc: Allow growing WallSection if the requested size is too large

--- compiler/hash-alloc/src/lib.rs
+++ compiler/hash-alloc/src/lib.rs
@@ -5,23 +5,36 @@
 #![allow(dead_code)] // @@REMOVE
 #![feature(bench_black_box)]
 
+use core::fmt;
 use parking_lot::Mutex;
-use std::{alloc::Layout, cell::{Cell, RefCell, UnsafeCell}, ops::{Deref, DerefMut}, ptr::NonNull};
+use std::{
+    alloc::Layout,
+    cell::{Cell, UnsafeCell},
+    marker::PhantomData,
+    ops::{Deref, DerefMut},
+    ptr::NonNull,
+};
 
 /// @@TODO: maybe make the ARENA_PAGE_SIZE configurable via 'cfg'?
 /// 1MiB
-const BRICK_SIZE: usize = 1 << 20;
+const INITIAL_SECTION_SIZE: usize = 1 << 20;
 
 #[derive(Debug)]
-struct Brick {
-    data: [u8; BRICK_SIZE],
+struct WallSection {
+    data: Box<[u8]>,
     offset: Cell<usize>,
 }
 
-impl Brick {
-    fn new() -> Self {
+impl Default for WallSection {
+    fn default() -> Self {
+        WallSection::new(INITIAL_SECTION_SIZE)
+    }
+}
+
+impl WallSection {
+    fn new(capacity: usize) -> Self {
         Self {
-            data: [0; BRICK_SIZE],
+            data: vec![0; capacity].into_boxed_slice(),
             offset: Cell::new(0),
         }
     }
@@ -33,12 +46,7 @@ impl Brick {
 
             let old_offset = self.offset.get();
             let new_offset = old_offset + layout_size;
-            if new_offset >= BRICK_SIZE {
-                if layout_size > BRICK_SIZE {
-                    // This means we cannot even allocate this layout at all.
-                    panic!("Tried to allocate an object on the castle that is too large!");
-                }
-
+            if new_offset >= self.data.len() {
                 // We cannot allocate.
                 return None;
             }
@@ -54,103 +62,79 @@ impl Brick {
         };
         Some(ptr)
     }
-}
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Wref<'w, T: ?Sized>(&'w T);
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct Wmut<'w, T: ?Sized>(&'w mut T);
-
-impl<'w, T> Wmut<'w, T> {
-    pub fn as_wref(self) -> Wref<'w, T> {
-        Wref(self.0)
+    fn len(&self) -> usize {
+        self.data.len()
     }
 }
 
-impl<T> Deref for Wmut<'_, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        self.0
-    }
-}
-
-impl<T> DerefMut for Wmut<'_, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.0
-    }
-}
-
-impl<T> Deref for Wref<'_, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        self.0
-    }
-}
-
-pub type Wstr<'w> = Wref<'w, str>;
-pub type Wslice<'w, T> = Wref<'w, [T]>;
-
-struct Wall<'c> {
-    curr_brick: RefCell<Box<Brick>>,
-    past_bricks: &'c PastCastleBricks,
+#[derive(Debug)]
+pub struct Wall<'c> {
+    curr_section: UnsafeCell<WallSection>,
+    past_sections: &'c PastSections,
 }
 
 impl<'c> Wall<'c> {
+    fn new(past_sections: &'c PastSections) -> Self {
+        Self {
+            curr_section: UnsafeCell::new(WallSection::default()),
+            past_sections,
+        }
+    }
+
     fn alloc_raw(&self, layout: Layout) -> NonNull<u8> {
         loop {
-            match self.curr_brick.borrow().alloc(layout) {
+            let curr_section = unsafe { &*self.curr_section.get() };
+            match curr_section.alloc(layout) {
                 Some(result) => {
                     break result;
                 }
                 None => {
-                    let new_brick = Box::new(Brick::new());
-                    let old_brick = self.curr_brick.replace(new_brick);
-                    self.past_bricks.add_brick(old_brick);
+                    let new_brick = WallSection::new(curr_section.len().saturating_mul(2));
+                    let old_brick =
+                        std::mem::replace(unsafe { &mut *self.curr_section.get() }, new_brick);
+
+                    self.past_sections.add_brick(old_brick);
                     continue;
                 }
             }
         }
     }
 
-    /// Allocate some type onto the wall, returning a mutable reference to the item.
-    #[inline]
-    pub fn make<T>(&self, object: T) -> Wmut<'c, T> {
-        self.make_with(|| object)
-    }
-
-    /// Allocate some type onto the wall, returning a mutable reference to the item.
-    #[inline]
-    pub fn make_with<T>(&self, f: impl FnOnce() -> T) -> Wmut<'c, T> {
+    fn alloc_raw_value<T>(&self, value: T) -> NonNull<T> {
         let layout = Layout::new::<T>();
-        let mut ptr: NonNull<T> = self.alloc_raw(layout).cast();
+        let ptr: NonNull<T> = self.alloc_raw(layout).cast();
         unsafe {
-            *(ptr.as_ptr()) = f();
+            *(ptr.as_ptr()) = value;
         }
-        Wmut(unsafe { ptr.as_mut() })
+        ptr
+    }
+}
+
+impl Clone for Wall<'_> {
+    fn clone(&self) -> Self {
+        Self::new(self.past_sections)
     }
 }
 
 #[derive(Debug, Default)]
-struct PastCastleBricks {
-    past_bricks: Mutex<Vec<Box<Brick>>>,
+struct PastSections {
+    past_bricks: Mutex<Vec<WallSection>>,
 }
 
-impl PastCastleBricks {
+impl PastSections {
     fn new() -> Self {
         Self::default()
     }
 
-    fn add_brick(&self, brick: Box<Brick>) {
+    fn add_brick(&self, brick: WallSection) {
         self.past_bricks.lock().push(brick);
     }
 }
 
 #[derive(Debug, Default)]
 struct Castle {
-    past_bricks: PastCastleBricks,
+    past_sections: PastSections,
 }
 
 impl Castle {
@@ -159,10 +143,204 @@ impl Castle {
     }
 
     pub fn wall(&self) -> Wall {
-        Wall {
-            curr_brick: RefCell::new(Box::new(Brick::new())),
-            past_bricks: &self.past_bricks,
+        Wall::new(&self.past_sections)
+    }
+}
+
+pub struct Brick<'c, T> {
+    data: NonNull<T>,
+    castle_lifetime: PhantomData<&'c ()>,
+}
+
+impl<'c, T> Brick<'c, T> {
+    pub fn new(x: T, wall: &Wall<'c>) -> Self {
+        Self {
+            data: wall.alloc_raw_value(x),
+            castle_lifetime: PhantomData::default(),
         }
+    }
+}
+
+impl<'c, T: Clone> Brick<'c, T> {
+    pub fn clone_out(&self) -> T {
+        (*self).clone()
+    }
+}
+
+impl<T: PartialEq> PartialEq for Brick<'_, T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.deref() == other.deref()
+    }
+}
+
+impl<T: Eq> Eq for Brick<'_, T> {}
+
+impl<T> Deref for Brick<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { self.data.as_ref() }
+    }
+}
+
+impl<T: fmt::Debug> fmt::Debug for Brick<'_, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.deref().fmt(f)
+    }
+}
+
+unsafe impl<T: Sync> Sync for Brick<'_, T> {}
+unsafe impl<T: Send> Send for Brick<'_, T> {}
+
+pub struct Row<'c, T> {
+    data: NonNull<T>,
+    length: usize,
+    capacity: usize,
+    castle_lifetime: PhantomData<&'c ()>,
+}
+
+const ROW_INITIAL_REALLOC_SIZE: usize = 4;
+const ROW_REALLOC_MULTIPLIER: usize = 2;
+
+impl<'c, T> Row<'c, T> {
+    pub fn new(wall: &Wall<'c>) -> Self {
+        Self::with_capacity(0, wall)
+    }
+
+    pub fn with_capacity(initial_capacity: usize, wall: &Wall<'c>) -> Self {
+        let layout = Layout::array::<T>(initial_capacity).unwrap();
+        let data = wall.alloc_raw(layout);
+        Self {
+            data: data.cast(),
+            length: 0,
+            capacity: initial_capacity,
+            castle_lifetime: PhantomData::default(),
+        }
+    }
+
+    pub fn reserve(&mut self, new_capacity: usize, wall: &Wall<'c>) {
+        if new_capacity < self.capacity {
+            // no-op
+            return;
+        }
+
+        if new_capacity < self.length {
+            panic!("Tried to reallocate with a capacity smaller than length");
+        }
+
+        let layout = Layout::array::<T>(new_capacity).unwrap();
+        let new_data: NonNull<T> = wall.alloc_raw(layout).cast();
+
+        // Safety: Both ranges are valid because they originate from wall.alloc_raw.
+        unsafe {
+            std::ptr::copy_nonoverlapping(self.data.as_ptr(), new_data.as_ptr(), self.length);
+        }
+
+        self.data = new_data;
+        self.capacity = new_capacity;
+    }
+
+    pub fn push(&mut self, element: T, wall: &Wall<'c>) {
+        if self.capacity < self.length + 1 {
+            if self.capacity == 0 {
+                self.reserve(ROW_INITIAL_REALLOC_SIZE, wall);
+            } else {
+                let new_capacity = self.capacity * ROW_REALLOC_MULTIPLIER;
+                if new_capacity > isize::MAX as usize {
+                    panic!("Reallocation target capacity is too large");
+                }
+
+                self.reserve(new_capacity, wall);
+            }
+        }
+
+        unsafe { *self.data.as_ptr().offset(self.length as isize) = element };
+        self.length += 1;
+    }
+
+    pub fn from_iter<I>(iter: I, wall: &Wall<'c>) -> Self
+    where
+        I: IntoIterator<Item = T>,
+    {
+        let iter = iter.into_iter();
+        let (min_bound, max_bound) = iter.size_hint();
+        let initial_capacity = max_bound.unwrap_or(min_bound);
+
+        let mut row = Row::with_capacity(initial_capacity, wall);
+        for element in iter {
+            row.push(element, wall);
+        }
+
+        row
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+}
+
+impl<T: fmt::Debug> fmt::Debug for Row<'_, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Row ")?;
+        self.deref().fmt(f)
+    }
+}
+
+impl<T> Deref for Row<'_, T> {
+    type Target = [T];
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { std::slice::from_raw_parts(self.data.as_ptr() as *const T, self.length) }
+    }
+}
+
+impl<T> DerefMut for Row<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { std::slice::from_raw_parts_mut(self.data.as_ptr(), self.length) }
+    }
+}
+
+unsafe impl<T: Send> Send for Row<'_, T> {}
+
+pub struct BrickString<'c> {
+    inner: Row<'c, u8>,
+}
+
+impl<'c> BrickString<'c> {
+    pub fn new(value: &str, wall: &Wall<'c>) -> Self {
+        let mut brick_str = Self::with_capacity(value.len(), wall);
+        for v in value.bytes() {
+            brick_str.inner.push(v, wall);
+        }
+        brick_str
+    }
+
+    pub fn with_capacity(initial_capacity: usize, wall: &Wall<'c>) -> Self {
+        Self {
+            inner: Row::with_capacity(initial_capacity, wall),
+        }
+    }
+
+    pub fn reserve(&mut self, new_capacity: usize, wall: &Wall<'c>) {
+        self.inner.reserve(new_capacity, wall)
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.inner.capacity()
+    }
+}
+
+impl Deref for BrickString<'_> {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { std::str::from_utf8_unchecked(self.inner.deref()) }
+    }
+}
+
+impl fmt::Debug for BrickString<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.deref().fmt(f)
     }
 }
 
@@ -185,8 +363,8 @@ mod test {
 
     #[derive(Debug)]
     struct MyComplexStructWref<'w> {
-        a: Wref<'w, i32>,
-        b: Wref<'w, Wref<'w, [Wref<'w, i32>; 5]>>,
+        a: Brick<'w, i32>,
+        b: Brick<'w, Brick<'w, [Brick<'w, i32>; 5]>>,
     }
 
     impl MyComplexStructBoxed {
@@ -207,19 +385,20 @@ mod test {
     impl<'w> MyComplexStructWref<'w> {
         pub fn new(wall: &Wall<'w>) -> Self {
             Self {
-                a: wall.make(3).as_wref(),
-                b: wall
-                    .make(
-                        wall.make([
-                            wall.make(4).as_wref(),
-                            wall.make(5).as_wref(),
-                            wall.make(6).as_wref(),
-                            wall.make(7).as_wref(),
-                            wall.make(8).as_wref(),
-                        ])
-                        .as_wref(),
-                    )
-                    .as_wref(),
+                a: Brick::new(3, wall),
+                b: Brick::new(
+                    Brick::new(
+                        [
+                            Brick::new(4, wall),
+                            Brick::new(5, wall),
+                            Brick::new(6, wall),
+                            Brick::new(7, wall),
+                            Brick::new(8, wall),
+                        ],
+                        wall,
+                    ),
+                    wall,
+                ),
             }
         }
     }
@@ -253,8 +432,8 @@ mod test {
 
     #[test]
     fn alloc_test() {
-        let total_count = 200;
-        let total_threads = 2;
+        let total_count = 2000;
+        let total_threads = 20;
 
         println!(
             "Generating {} objects inside {} thread(s)",
@@ -292,5 +471,19 @@ mod test {
             elapsed,
             elapsed / total_count as u32
         );
+    }
+
+    #[test]
+    fn row_test() {
+        let castle = Castle::new();
+        let wall = castle.wall();
+        let mut row = Row::<i64>::new(&wall);
+        row.reserve(10000000, &wall);
+
+        for i in 0..10000000 {
+            row.push(i, &wall);
+        }
+
+        println!("{:?}", row);
     }
 }
