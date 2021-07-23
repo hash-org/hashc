e345cc9e87f18b875e2351f5343d931bf6c4fd8b
Constantine Theocharis <cthe@mailbox.org>
2021-07-23 14:58:35 +0000

    Alloc: add memory block reclaiming to reduce the amount of wasted bytes

--- compiler/hash-alloc/src/lib.rs
+++ compiler/hash-alloc/src/lib.rs
@@ -4,13 +4,14 @@
 
 #![allow(dead_code)] // @@REMOVE
 #![feature(bench_black_box)]
+#![feature(maybe_uninit_extra)]
 
 use core::fmt;
 use parking_lot::Mutex;
 use std::{
     alloc::Layout,
     cell::{Cell, UnsafeCell},
-    marker::PhantomData,
+    mem::{ManuallyDrop, MaybeUninit},
     ops::{Deref, DerefMut},
     ptr::NonNull,
 };
@@ -19,34 +20,37 @@ use std::{
 /// 1MiB
 const INITIAL_SECTION_SIZE: usize = 1 << 20;
 
+const RECLAIM_SECTION_MIN_REMAINING: usize = 1 << 8;
+
 #[derive(Debug)]
 struct WallSection {
-    data: Box<[u8]>,
+    data: UnsafeCell<Box<[MaybeUninit<u8>]>>,
     offset: Cell<usize>,
 }
 
-impl Default for WallSection {
-    fn default() -> Self {
-        WallSection::new(INITIAL_SECTION_SIZE)
-    }
-}
-
 impl WallSection {
-    fn new(capacity: usize) -> Self {
+    fn new() -> Self {
+        WallSection::with_capacity(INITIAL_SECTION_SIZE)
+    }
+
+    fn with_capacity(capacity: usize) -> Self {
         Self {
-            data: vec![0; capacity].into_boxed_slice(),
+            data: UnsafeCell::new(vec![MaybeUninit::uninit(); capacity].into_boxed_slice()),
             offset: Cell::new(0),
         }
     }
 
-    fn alloc(&self, layout: Layout) -> Option<NonNull<u8>> {
+    fn alloc(&self, layout: Layout) -> Option<NonNull<MaybeUninit<u8>>> {
         // Old value is start
         let start = {
             let layout_size = layout.size();
 
             let old_offset = self.offset.get();
             let new_offset = old_offset + layout_size;
-            if new_offset >= self.data.len() {
+
+            // Safety: no one has access to data right now.
+            let max_offset = unsafe { &*self.data.get() }.len();
+            if new_offset >= max_offset {
                 // We cannot allocate.
                 return None;
             }
@@ -55,34 +59,76 @@ impl WallSection {
             old_offset
         };
 
+        // Safety: no one has access to data right now, previous reference (max_offset) has been
+        // dropped.
+        let data_ptr = unsafe { &mut *self.data.get() }.as_mut_ptr();
+
         // Pointer to return is `start` offset from self.data.
         // Safety: we have ensured that the layout fits on the brick.
+        //
+        // Safety: ranges are never overlapping, so we don't need to care about aliasing. We can
+        // safely return this pointer, given that the user only accesses data within
+        //  0..layout.size()
         let ptr = unsafe {
-            NonNull::new_unchecked(self.data.as_ptr().offset(start as isize) as *mut u8)
+            // @@TODO: what about integer overflow on start?
+            NonNull::new_unchecked(data_ptr.offset(start as isize))
         };
         Some(ptr)
     }
 
-    fn len(&self) -> usize {
-        self.data.len()
+    fn allocated_bytes(&self) -> usize {
+        // Safety: no one has reference access to data length.
+        unsafe { &*self.data.get() }.len()
+    }
+
+    fn used_bytes(&self) -> usize {
+        self.offset.get()
+    }
+
+    fn remaining_bytes(&self) -> usize {
+        self.allocated_bytes() - self.used_bytes()
     }
 }
 
 #[derive(Debug)]
 pub struct Wall<'c> {
-    curr_section: UnsafeCell<WallSection>,
-    past_sections: &'c PastSections,
+    curr_section: ManuallyDrop<UnsafeCell<WallSection>>,
+    castle: &'c Castle,
 }
 
 impl<'c> Wall<'c> {
-    fn new(past_sections: &'c PastSections) -> Self {
+    pub fn new(castle: &'c Castle) -> Self {
         Self {
-            curr_section: UnsafeCell::new(WallSection::default()),
-            past_sections,
+            curr_section: ManuallyDrop::new(UnsafeCell::new(WallSection::new())),
+            castle,
         }
     }
 
-    fn alloc_raw(&self, layout: Layout) -> NonNull<u8> {
+    pub fn with_capacity(capacity: usize, castle: &'c Castle) -> Self {
+        Self {
+            curr_section: ManuallyDrop::new(UnsafeCell::new(WallSection::with_capacity(capacity))),
+            castle,
+        }
+    }
+
+    pub fn allocated_bytes(&self) -> usize {
+        // Safety: no one else has a mutable reference to curr_section, and we are only reading
+        unsafe { &*self.curr_section.get() }.allocated_bytes()
+    }
+
+    pub fn used_bytes(&self) -> usize {
+        // Safety: no one else has a mutable reference to curr_section, and we are only reading
+        unsafe { &*self.curr_section.get() }.used_bytes()
+    }
+
+    fn with_section(section: WallSection, castle: &'c Castle) -> Self {
+        Self {
+            curr_section: ManuallyDrop::new(UnsafeCell::new(section)),
+            castle,
+        }
+    }
+
+    fn alloc_raw(&self, layout: Layout) -> NonNull<MaybeUninit<u8>> {
         loop {
             let curr_section = unsafe { &*self.curr_section.get() };
             match curr_section.alloc(layout) {
@@ -90,36 +136,66 @@ impl<'c> Wall<'c> {
                     break result;
                 }
                 None => {
-                    let new_brick = WallSection::new(curr_section.len().saturating_mul(2));
-                    let old_brick =
-                        std::mem::replace(unsafe { &mut *self.curr_section.get() }, new_brick);
+                    // New capacity is largest of: requested size, or current allocated bytes * 2.
+                    // @@Improvement: potential for improvement in estimation?
+                    let new_capacity = usize::max(
+                        layout.size(),
+                        curr_section.allocated_bytes().saturating_mul(2),
+                    );
 
-                    self.past_sections.add_brick(old_brick);
+                    let new_wall_section = self
+                        .castle
+                        .new_or_reclaimed_section_with_capacity(new_capacity);
+
+                    // Drop read access to curr_section.
+                    drop(curr_section);
+
+                    // Safety: no one has read access to self.curr_section right now.
+                    let old_wall_section = std::mem::replace(
+                        unsafe { &mut *self.curr_section.get() },
+                        new_wall_section,
+                    );
+
+                    self.castle.reclaim_section(old_wall_section);
                     continue;
                 }
             }
         }
     }
 
-    fn alloc_raw_value<T>(&self, value: T) -> NonNull<T> {
+    fn alloc_value<T>(&self, value: T) -> &'c mut T {
         let layout = Layout::new::<T>();
-        let ptr: NonNull<T> = self.alloc_raw(layout).cast();
+        let mut ptr: NonNull<MaybeUninit<T>> = self.alloc_raw(layout).cast();
         unsafe {
-            *(ptr.as_ptr()) = value;
+            *(ptr.as_ptr()) = MaybeUninit::new(value);
         }
-        ptr
+        unsafe { ptr.as_mut().assume_init_mut() }
+    }
+
+    fn alloc_uninit_slice<T>(&self, len: usize) -> &'c mut [MaybeUninit<T>] {
+        let layout = Layout::array::<T>(len).unwrap();
+        let ptr: NonNull<MaybeUninit<T>> = self.alloc_raw(layout).cast();
+        unsafe { std::slice::from_raw_parts_mut(ptr.as_ptr(), len) }
+    }
+}
+
+impl Drop for Wall<'_> {
+    fn drop(&mut self) {
+        // Reclaim the last section
+        let section = unsafe { ManuallyDrop::take(&mut self.curr_section) }.into_inner();
+        self.castle.reclaim_section(section);
     }
 }
 
 impl Clone for Wall<'_> {
     fn clone(&self) -> Self {
-        Self::new(self.past_sections)
+        self.castle.wall()
     }
 }
 
 #[derive(Debug, Default)]
 struct PastSections {
-    past_bricks: Mutex<Vec<WallSection>>,
+    past_sections: Mutex<Vec<WallSection>>,
 }
 
 impl PastSections {
@@ -127,14 +203,35 @@ impl PastSections {
         Self::default()
     }
 
-    fn add_brick(&self, brick: WallSection) {
-        self.past_bricks.lock().push(brick);
+    fn add_section(&self, brick: WallSection) {
+        self.past_sections.lock().push(brick);
+    }
+
+    fn take_section(&self) -> Option<WallSection> {
+        self.past_sections.lock().pop()
+    }
+
+    fn allocated_bytes(&self) -> usize {
+        self.past_sections
+            .lock()
+            .iter()
+            .map(|sec| sec.allocated_bytes())
+            .sum()
+    }
+
+    fn used_bytes(&self) -> usize {
+        self.past_sections
+            .lock()
+            .iter()
+            .map(|sec| sec.used_bytes())
+            .sum()
     }
 }
 
 #[derive(Debug, Default)]
-struct Castle {
-    past_sections: PastSections,
+pub struct Castle {
+    full_sections: PastSections,
+    reclaimable_sections: PastSections,
 }
 
 impl Castle {
@@ -142,21 +239,53 @@ impl Castle {
         Self::default()
     }
 
+    pub fn wall_with_capacity(&self, capacity: usize) -> Wall {
+        Wall::with_section(self.new_or_reclaimed_section_with_capacity(capacity), self)
+    }
+
     pub fn wall(&self) -> Wall {
-        Wall::new(&self.past_sections)
+        Wall::with_section(self.new_or_reclaimed_section(), self)
+    }
+
+    pub fn past_allocated_bytes(&self) -> usize {
+        self.full_sections.allocated_bytes() + self.reclaimable_sections.allocated_bytes()
+    }
+
+    pub fn past_used_bytes(&self) -> usize {
+        self.full_sections.used_bytes() + self.reclaimable_sections.used_bytes()
+    }
+
+    fn new_or_reclaimed_section_with_capacity(&self, capacity: usize) -> WallSection {
+        match self.reclaimable_sections.take_section() {
+            Some(section) if section.remaining_bytes() >= capacity => section,
+            _ => WallSection::with_capacity(capacity),
+        }
+    }
+
+    fn new_or_reclaimed_section(&self) -> WallSection {
+        match self.reclaimable_sections.take_section() {
+            Some(section) => section,
+            None => WallSection::new(),
+        }
+    }
+
+    fn reclaim_section(&self, section: WallSection) {
+        if section.remaining_bytes() >= RECLAIM_SECTION_MIN_REMAINING {
+            self.reclaimable_sections.add_section(section);
+        } else {
+            self.full_sections.add_section(section);
+        }
     }
 }
 
 pub struct Brick<'c, T> {
-    data: NonNull<T>,
-    castle_lifetime: PhantomData<&'c ()>,
+    data: &'c mut T,
 }
 
 impl<'c, T> Brick<'c, T> {
     pub fn new(x: T, wall: &Wall<'c>) -> Self {
         Self {
-            data: wall.alloc_raw_value(x),
-            castle_lifetime: PhantomData::default(),
+            data: wall.alloc_value(x),
         }
     }
 }
@@ -179,7 +308,13 @@ impl<T> Deref for Brick<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        unsafe { self.data.as_ref() }
+        self.data
+    }
+}
+
+impl<T> DerefMut for Brick<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.data
     }
 }
 
@@ -189,18 +324,10 @@ impl<T: fmt::Debug> fmt::Debug for Brick<'_, T> {
     }
 }
 
-unsafe impl<T: Sync> Sync for Brick<'_, T> {}
-unsafe impl<T: Send> Send for Brick<'_, T> {}
-
 pub struct Row<'c, T> {
-    data: NonNull<T>,
+    data: &'c mut [MaybeUninit<T>],
     length: usize,
-    capacity: usize,
-    castle_lifetime: PhantomData<&'c ()>,
 }
-
-unsafe impl<T: Sync> Sync for Row<'_, T> {}
-unsafe impl<T: Send> Send for Row<'_, T> {}
 
 const ROW_INITIAL_REALLOC_SIZE: usize = 4;
 const ROW_REALLOC_MULTIPLIER: usize = 2;
@@ -211,44 +338,44 @@ impl<'c, T> Row<'c, T> {
     }
 
     pub fn with_capacity(initial_capacity: usize, wall: &Wall<'c>) -> Self {
-        let layout = Layout::array::<T>(initial_capacity).unwrap();
-        let data = wall.alloc_raw(layout);
         Self {
-            data: data.cast(),
+            data: wall.alloc_uninit_slice(initial_capacity),
             length: 0,
-            capacity: initial_capacity,
-            castle_lifetime: PhantomData::default(),
         }
     }
 
+    pub fn capacity(&self) -> usize {
+        self.data.len()
+    }
+
     pub fn reserve(&mut self, new_capacity: usize, wall: &Wall<'c>) {
-        if new_capacity < self.capacity {
+        if new_capacity < self.capacity() {
             // no-op
             return;
         }
 
-        if new_capacity < self.length {
+        if new_capacity < self.len() {
             panic!("Tried to reallocate with a capacity smaller than length");
         }
 
-        let layout = Layout::array::<T>(new_capacity).unwrap();
-        let new_data: NonNull<T> = wall.alloc_raw(layout).cast();
+        println!("Reserving {}, current {}", new_capacity, self.capacity());
+        let new_data = wall.alloc_uninit_slice(new_capacity);
 
         // Safety: Both ranges are valid because they originate from wall.alloc_raw.
+        // NB: self.data is now invalid, elements have been moved.
         unsafe {
-            std::ptr::copy_nonoverlapping(self.data.as_ptr(), new_data.as_ptr(), self.length);
+            std::ptr::copy_nonoverlapping(self.data.as_ptr(), new_data.as_mut_ptr(), self.len());
         }
 
         self.data = new_data;
-        self.capacity = new_capacity;
     }
 
     pub fn push(&mut self, element: T, wall: &Wall<'c>) {
-        if self.capacity < self.length + 1 {
-            if self.capacity == 0 {
+        if self.capacity() < self.len() + 1 {
+            if self.capacity() == 0 {
                 self.reserve(ROW_INITIAL_REALLOC_SIZE, wall);
             } else {
-                let new_capacity = self.capacity * ROW_REALLOC_MULTIPLIER;
+                let new_capacity = self.capacity() * ROW_REALLOC_MULTIPLIER;
                 if new_capacity > isize::MAX as usize {
                     panic!("Reallocation target capacity is too large");
                 }
@@ -257,8 +384,23 @@ impl<'c, T> Row<'c, T> {
             }
         }
 
-        unsafe { *self.data.as_ptr().offset(self.length as isize) = element };
+        // Safety: we just reserved enough for this to be valid.
+        *unsafe { self.data.get_unchecked_mut(self.len()) } = MaybeUninit::new(element);
         self.length += 1;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.len() == 0 {
+            return None;
+        }
+
+        let last_element = std::mem::replace(
+            unsafe { self.data.get_unchecked_mut(self.len() - 1) },
+            MaybeUninit::uninit(),
+        );
+        self.length -= 1;
+
+        Some(unsafe { last_element.assume_init() })
     }
 
     pub fn from_iter<I>(iter: I, wall: &Wall<'c>) -> Self
@@ -276,12 +418,9 @@ impl<'c, T> Row<'c, T> {
 
         row
     }
-
-    pub fn capacity(&self) -> usize {
-        self.capacity
-    }
 }
 
+#[macro_export]
 macro_rules! row {
     ($wall:expr) => { Row::new($wall) };
     ($wall:expr; $($item:expr),*) => { Row::from_iter([$($item,)*], $wall) };
@@ -299,13 +438,28 @@ impl<T> Deref for Row<'_, T> {
     type Target = [T];
 
     fn deref(&self) -> &Self::Target {
-        unsafe { std::slice::from_raw_parts(self.data.as_ptr() as *const T, self.length) }
+        // Safety: values until self.length are initialised.
+        unsafe { std::mem::transmute::<&[MaybeUninit<T>], &[T]>(&self.data[0..self.length]) }
     }
 }
 
 impl<T> DerefMut for Row<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { std::slice::from_raw_parts_mut(self.data.as_ptr(), self.length) }
+        // Safety: values until self.length are initialised.
+        unsafe {
+            std::mem::transmute::<&mut [MaybeUninit<T>], &mut [T]>(&mut self.data[0..self.length])
+        }
+    }
+}
+
+impl<T> Drop for Row<'_, T> {
+    fn drop(&mut self) {
+        // Drop elements until self.length
+        for x in (&mut self.data[0..self.length]).into_iter() {
+            unsafe {
+                x.assume_init_drop();
+            }
+        }
     }
 }
 
@@ -482,12 +636,80 @@ mod test {
         let castle = Castle::new();
         let wall = castle.wall();
 
-        let row: Row<i32> = row![&wall];
-        let row2 = row![&wall; 1, 2, 3];
-        let row3 = row![&wall; 10; 500];
+        println!(
+            "used {}, total {}",
+            wall.used_bytes(),
+            wall.allocated_bytes()
+        );
 
-        println!("{:?}", row);
-        println!("{:?}", row2);
-        println!("{:?}", row3);
+        let row: Row<i32> = row![&wall];
+
+        println!(
+            "used {}, total {}",
+            wall.used_bytes(),
+            wall.allocated_bytes()
+        );
+        let mut row2 = row![&wall; 1, 2, 3];
+
+        println!(
+            "used {}, total {}",
+            wall.used_bytes(),
+            wall.allocated_bytes()
+        );
+        let mut row3 = row![&wall; 10; 5000000];
+
+        println!(
+            "used {}, total {}",
+            wall.used_bytes(),
+            wall.allocated_bytes()
+        );
+
+        row3.push(4, &wall);
+        let el = row2.pop();
+        println!("{:?}", el);
+
+        println!(
+            "used {}, total {}",
+            wall.used_bytes(),
+            wall.allocated_bytes()
+        );
+        let el = row2.pop();
+        println!("{:?}", el);
+
+        println!(
+            "used {}, total {}",
+            wall.used_bytes(),
+            wall.allocated_bytes()
+        );
+        let el = row2.pop();
+        println!("{:?}", el);
+
+        println!(
+            "used {}, total {}",
+            wall.used_bytes(),
+            wall.allocated_bytes()
+        );
+        let el = row2.pop();
+        println!("{:?}", el);
+
+        println!(
+            "used {}, total {}",
+            wall.used_bytes(),
+            wall.allocated_bytes()
+        );
+        let el = row2.pop();
+        println!("{:?}", el);
+
+        println!(
+            "used {}, total {}",
+            wall.used_bytes(),
+            wall.allocated_bytes()
+        );
+
+        println!(
+            "castle past used {}, total {}",
+            castle.past_used_bytes(),
+            castle.past_allocated_bytes()
+        );
     }
 }
