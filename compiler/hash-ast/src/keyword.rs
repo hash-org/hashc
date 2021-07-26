ce6ca8527ecd57ff51667cbc9df12a42f0ff36d1
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2021-07-26 13:37:56 +0100

    Tokeniser: Use strum to inject keywords into the identifier map

--- compiler/hash-ast/src/keyword.rs
+++ compiler/hash-ast/src/keyword.rs
@@ -3,8 +3,10 @@
 //! All rights reserved 2021 (c) The Hash Language authors
 
 use std::fmt;
-use std::slice::Iter;
+use strum_macros::{AsRefStr, EnumString, EnumVariantNames};
 
+#[derive(Debug, PartialEq, AsRefStr, EnumString, EnumVariantNames)]
+#[strum(serialize_all = "snake_case")]
 pub enum Keyword {
     Let,
     For,
@@ -24,42 +26,8 @@ pub enum Keyword {
     Return,
 }
 
-impl Keyword {
-    pub fn iterator() -> Iter<'static, Keyword> {
-        #[rustfmt::skip]
-        static DIRECTIONS: [Keyword; 16] = [
-            Keyword::Let, Keyword::For, Keyword::While, Keyword::Loop,
-            Keyword::If, Keyword::Else, Keyword::Match, Keyword::As,
-            Keyword::In, Keyword::Where, Keyword::Trait, Keyword::Enum,
-            Keyword::Struct, Keyword::Continue, Keyword::Break, Keyword::Return,
-        ];
-        DIRECTIONS.iter()
-    }
-
-    pub fn to_str(&self) -> &str {
-        match self {
-            Keyword::As => "as",
-            Keyword::In => "in",
-            Keyword::Let => "let",
-            Keyword::Match => "match",
-            Keyword::For => "for",
-            Keyword::While => "while",
-            Keyword::Loop => "loop",
-            Keyword::If => "if",
-            Keyword::Else => "else",
-            Keyword::Where => "where",
-            Keyword::Trait => "trait",
-            Keyword::Enum => "enum",
-            Keyword::Struct => "struct",
-            Keyword::Continue => "continue",
-            Keyword::Break => "break",
-            Keyword::Return => "return",
-        }
-    }
-}
-
 impl fmt::Display for Keyword {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.to_str())
+        write!(f, "{}", self.as_ref())
     }
 }
