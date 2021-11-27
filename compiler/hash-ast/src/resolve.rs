fa2199a8fc6e394762fed2531256db1fb244b9ef
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2021-11-27 10:47:56 +0000

    fix: Compiler panic when file couldn't be parsed.

--- compiler/hash-ast/src/resolve.rs
+++ compiler/hash-ast/src/resolve.rs
@@ -122,13 +122,15 @@ where
                     |elapsed| println!("ast: {:.2?}", elapsed),
                 );
 
+                // @@Cleanup:
+                // @@Hack: we still need to add the contents of the file into the file map whilst the parser
+                //         is parsing. If we don't do this, the reporting crate won't be able to access the 
+                //         contents of the file for reporting purposes.
+                ctx.module_builder
+                    .add_contents(import_index, resolved_import_path, import_source);
+
                 // Add the import to modules
-                ctx.module_builder.add_module_at(
-                    import_index,
-                    resolved_import_path,
-                    import_source,
-                    import_node?,
-                );
+                ctx.module_builder.add_module_at(import_index, import_node?);
 
                 Ok(())
             });
