b9883848996f6f711eb18f55a71a53a4dc30e84a
Constantine Theocharis <cthe@mailbox.org>
2021-12-27 21:31:30 +0000

    Move TypecheckError to its own module

---
+++ compiler/hash-typecheck/src/error.rs
@@ -0,0 +1,21 @@
+use hash_ast::{ast::TypeId, ident::Identifier, location::Location};
+
+#[derive(Debug)]
+pub enum TypecheckError {
+    TypeMismatch(TypeId, TypeId),
+    UsingBreakOutsideLoop(Location),
+    UsingContinueOutsideLoop(Location),
+    UsingReturnOutsideFunction(Location),
+    RequiresIrrefutablePattern(Location),
+    UnresolvedSymbol(Vec<Identifier>),
+    TryingToNamespaceType(Vec<Identifier>),
+    TryingToNamespaceVariable(Vec<Identifier>),
+    UsingVariableInTypePos(Vec<Identifier>),
+    UsingTypeInVariablePos(Vec<Identifier>),
+    InvalidPropertyAccess(TypeId, Identifier),
+    ExpectingBooleanInCondition { found: TypeId },
+    // @@Todo: turn this into variants
+    Message(String),
+}
+
+pub type TypecheckResult<T> = Result<T, TypecheckError>;
