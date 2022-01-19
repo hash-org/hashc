211ecd931482dc296bb81fcecaa6a7485a84153c
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-01-19 10:33:22 -0500

    reporting: add ErrorCode enum in hash-error-codes

--- compiler/hash-error-codes/src/error_codes.rs
+++ compiler/hash-error-codes/src/error_codes.rs
@@ -1,13 +1,44 @@
-register_diagnostics! {
-    ;
-    E0001,
-    E0002,
-    E0003,
-    E0004,
-    E0005,
-    E0006,
-    E0007,
-    E0008,
-    E0009,
-    E00010,
+macro_rules! error_codes {
+    ($($name:ident = $code:expr,)*) => (
+        #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+        #[allow(dead_code)]
+        pub enum HashErrorCode {
+            $($name, )*
+        }
+
+        impl HashErrorCode {
+            #[allow(unused)]
+            pub fn to_num(&self) -> u32 {
+                match self {
+                    $(Self::$name => $code, )*
+                }
+            }
+        }
+    )
+}
+
+error_codes! {
+    TypeMismatch = 1,
+    UsingBreakOutsideLoop = 2,
+    UsingContinueOutsideLoop = 3,
+    UsingReturnOutsideFunction = 4,
+    RequiresIrrefutablePattern = 5,
+    UnresolvedSymbol = 6,
+    TryingToNamespaceType = 7,
+    TryingToNamespaceVariable = 8,
+    SymbolIsNotAType = 9,
+    SymbolIsNotAVariable = 10,
+    SymbolIsNotATrait = 11,
+    TypeIsNotStruct = 12,
+    UnresolvedStructField = 13,
+    InvalidPropertyAccess = 14,
+    ExpectingBooleanInCondition = 15,
+    MissingStructField = 16,
+    BoundRequiresStrictlyTypeVars = 17,
+    ExpectingBindingForTraitImpl = 18,
+    TraitDefinitionNotFound = 19,
+    TypeAnnotationNotAllowedInTraitImpl = 20,
+    TypeArgumentLengthMismatch = 21,
+    NoMatchingTraitImplementations = 22,
+    FunctionArgumentLengthMismatch = 23,
 }
--- compiler/hash-error-codes/src/lib.rs
+++ compiler/hash-error-codes/src/lib.rs
@@ -1,22 +1,24 @@
-macro_rules! register_diagnostics {
-    ($($ecode:ident: $message:expr,)* ; $($code:ident,)*) => (
-        pub static DIAGNOSTICS: &[(&str, Option<&str>)] = &[
-            $( (stringify!($ecode), Some($message)), )*
-            $( (stringify!($code), None), )*
-        ];
-    )
-}
+// macro_rules! register_diagnostics {
+//     ($($ecode:ident: $message:expr,)* ; $($code:ident,)*) => (
+//         pub static DIAGNOSTICS: &[(&str, Option<&str>)] = &[
+//             $( (stringify!($ecode), Some($message)), )*
+//             $( (stringify!($code), None), )*
+//         ];
+//     )
+// }
 
-pub enum DiagnosticId {
-    Error(String),
-}
+// pub enum DiagnosticId {
+//     Error(String),
+// }
 
-mod error_codes;
-pub use error_codes::DIAGNOSTICS;
+// mod error_codes;
+// pub use error_codes::DIAGNOSTICS;
 
-#[macro_export]
-macro_rules! error_code {
-    ($code:ident) => {{
-        $crate::DiagnosticId::Error(stringify!($code).to_owned())
-    }};
-}
+// #[macro_export]
+// macro_rules! error_code {
+//     ($code:ident) => {{
+//         $crate::DiagnosticId::Error(stringify!($code).to_owned())
+//     }};
+// }
+
+pub mod error_codes;
