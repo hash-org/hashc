720916eda5f69085b404d8e03d40828425e797ac
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-02-22 21:37:35 +0000

    vm: Implement floating point arithmetic instructions

--- compiler/hash-vm/src/error.rs
+++ compiler/hash-vm/src/error.rs
@@ -1,28 +1,42 @@
-use hash_reporting::reporting::{Report, ReportKind};
+use std::fmt;
 
+use hash_reporting::reporting::{Report, ReportKind};
 
 pub enum StackAccessKind {
     Push,
-    Pop
+    Pop,
+}
+
+impl fmt::Display for StackAccessKind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            StackAccessKind::Push => write!(f, "push"),
+            StackAccessKind::Pop => write!(f, "pop"),
+        }
+    }
 }
 
 pub enum RuntimeError {
     StackViolationAccess {
         kind: StackAccessKind,
         size: u8,
-        total: usize
-    }
+        total: usize,
+    },
 }
 
 pub type RuntimeResult<T> = Result<T, RuntimeError>;
 
 impl From<RuntimeError> for Report {
-    fn from(_: RuntimeError) -> Self {
-        Report {
-            kind: ReportKind::Error,
-            message: "Failed to execute".to_string(),
-            error_code: None,
-            contents: vec![],
+    fn from(err: RuntimeError) -> Self {
+        match err {
+            RuntimeError::StackViolationAccess { kind, size, total } => {
+                Report {
+                    kind: ReportKind::Error,
+                    message: format!("Stack access violation occurred: tried to `{}` {}bytes from stack, but stack stack size is {}", kind, size, total ),
+                    error_code: None,
+                    contents: vec![],
+                }
+            },
         }
     }
 }
