44c3c05d93c6163ae3c561f9e7bc715149897c7f
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-05-06 18:09:09 +0100

    Compiler: extract lexer component from parser

--- compiler/hash-lexer/src/utils.rs
+++ compiler/hash-lexer/src/utils.rs
@@ -3,13 +3,13 @@
 //! All rights reserved 2022 (c) The Hash Language authors
 
 /// True if `c` is valid as a first character of an identifier.
-// @@ Future: Support unicode within idents?
+// @@Future: Support unicode within idents?
 pub(crate) fn is_id_start(c: char) -> bool {
     ('a'..='z').contains(&c) || ('A'..='Z').contains(&c) || c == '_'
 }
 
 /// True if `c` is valid as a non-first character of an identifier.
-// @@ Future: Support unicode within idents?
+// @@Future: Support unicode within idents?
 pub(crate) fn is_id_continue(c: char) -> bool {
     ('a'..='z').contains(&c) || ('A'..='Z').contains(&c) || ('0'..='9').contains(&c) || c == '_'
 }
--- compiler/hash-parser/src/lib.rs
+++ compiler/hash-parser/src/lib.rs
@@ -1,11 +1,8 @@
-//! Parser library lib file.
+//! Hash Compiler Parser crate.
 //!
 //! All rights reserved 2022 (c) The Hash Language authors
 #![feature(cell_update)]
 
 pub mod error;
 pub mod gen;
-pub mod lexer;
 pub mod parser;
-pub mod token;
-mod utils;
