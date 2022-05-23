74cd38ffbaad48e291b4088821451687c4050b7f
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-05-23 17:39:23 +0300

    Parser: support full syntax of type function definition arguments

--- compiler/hash-parser/src/parser/definitions.rs
+++ compiler/hash-parser/src/parser/definitions.rs
@@ -3,6 +3,7 @@
 //!
 //! All rights reserved 2022 (c) The Hash Language authors
 
+use hash_alloc::row;
 use hash_ast::ast::*;
 use hash_token::{delimiter::Delimiter, keyword::Keyword, Token, TokenKind, TokenKindVector};
 
@@ -133,18 +134,81 @@ impl<'c, 'stream, 'resolver> AstGen<'c, 'stream, 'resolver> {
     /// Parse a [TypeFunctionDef]. Type functions specify logic at the type level on expressions such as
     /// struct, enum, function, and trait definitions.
     pub fn parse_type_function_def(&self) -> AstGenResult<'c, TypeFunctionDef<'c>> {
-        // @@Hack: Since we already parsed the `<`, we need to notify the
-        //         type_args parser function that it doesn't need to parse this
-        let type_args = self.parse_type_args(true)?;
+        let mut args = AstNodes::empty();
+
+        while let Some(arg) = self.peek_resultant_fn(|| self.parse_type_function_def_arg()) {
+            args.nodes.push(arg, &self.wall);
+
+            match self.peek() {
+                Some(token) if token.has_kind(TokenKind::Comma) => {
+                    self.skip_token();
+                }
+                Some(token) if token.has_kind(TokenKind::Gt) => {
+                    self.skip_token();
+                    break;
+                }
+                token => self.error_with_location(
+                    AstGenErrorKind::Expected,
+                    Some(TokenKindVector::from_row(row![
+                        &self.wall;
+                        TokenKind::Comma,
+                        TokenKind::Gt
+                    ])),
+                    token.map(|t| t.kind),
+                    token.map_or_else(|| self.next_location(), |t| t.span),
+                )?,
+            }
+        }
+
+        // We can't do this because the parse_separated_fn() function expects a token tree and
+        // not the while tree:
+        //
+        // let args = self.parse_separated_fn(
+        //     || self.parse_type_function_def_arg(),
+        //     || self.parse_token_atom(TokenKind::Comma),
+        // )?;
 
         // Now that we parse the bound, we're expecting a fat-arrow and then some expression
         self.parse_arrow()?;
         let expr = self.parse_expression_with_precedence(0)?;
 
-        Ok(TypeFunctionDef {
-            args: type_args,
-            expr,
-        })
+        Ok(TypeFunctionDef { args, expr })
+    }
+
+    // Parse a [TypeFunctionDefArg] which consists the name of the argument and then any specified bounds
+    // on the argument which are essentially types that are separated by a `~`
+    fn parse_type_function_def_arg(&self) -> AstGenResult<'c, AstNode<'c, TypeFunctionDefArg<'c>>> {
+        let start = self.current_location();
+        let name = self.parse_name()?;
+
+        // Now it's followed by a colon
+        self.parse_token_atom(TokenKind::Colon)?;
+
+        // Parse any bounds present
+        let mut bounds = AstNodes::empty();
+
+        loop {
+            match self.peek_resultant_fn(|| self.parse_type()) {
+                Some(ty) => {
+                    bounds.nodes.push(ty, &self.wall);
+
+                    match self.peek() {
+                        Some(token) if token.has_kind(TokenKind::Tilde) => {
+                            self.skip_token();
+                        }
+                        _ => break,
+                    }
+                }
+                None => self.error_with_location(
+                    AstGenErrorKind::ExpectedType,
+                    None,
+                    None,
+                    self.next_location(),
+                )?,
+            }
+        }
+
+        Ok(self.node_with_joined_span(TypeFunctionDefArg { name, bounds }, &start))
     }
 
     /// Parse a [TraitDef]. A [TraitDef] is essentially a block prefixed with `trait` that contains
@@ -163,10 +227,19 @@ impl<'c, 'stream, 'resolver> AstGen<'c, 'stream, 'resolver> {
                 let tree = self.token_trees.get(*tree_index).unwrap();
                 let gen = self.from_stream(tree, *span);
 
-                gen.parse_separated_fn(
-                    || gen.parse_expression(),
-                    || gen.parse_token_atom(TokenKind::Semi),
-                )?
+                let mut expressions = AstNodes::empty();
+
+                while gen.has_token() {
+                    let (_, expr) = gen.parse_top_level_expression(true)?;
+                    expressions.nodes.push(expr, &self.wall);
+                }
+
+                // Verify that generator is empty
+                if gen.has_token() {
+                    gen.error(AstGenErrorKind::EOF, None, None)?
+                }
+
+                expressions
             }
             token => self.error_with_location(
                 AstGenErrorKind::Expected,
