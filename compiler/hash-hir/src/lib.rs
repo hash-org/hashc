19706aa896c4bc0b170acb320be47644f3b75eb7
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-06-01 15:26:08 +0100

    chore(deps): drop unecessary crate dependencies

--- compiler/hash-hir/src/lib.rs
+++
@@ -1,3 +0,0 @@
-//! Hash compiler Higher Intermediate Representation (HIR) crate.
-//!
-//! All rights reserved 2021 (c) The Hash Language authors
---
+++ compiler/hash-ir/src/lib.rs
@@ -0,0 +1,6 @@
+//! Hash Compiler Intermediate Representation (IR) crate.
+//!
+//! All rights reserved 2021 (c) The Hash Language authors
+
+pub mod ir;
+pub mod visitor;
