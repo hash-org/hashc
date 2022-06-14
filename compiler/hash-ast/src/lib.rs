1549febe10bc7af1e0767ba5b2428c5b8e892907
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-06-14 13:59:48 +0100

    chore(deps): drop unecessary crate dependencies

--- compiler/hash-ast/src/lib.rs
+++ compiler/hash-ast/src/lib.rs
@@ -3,14 +3,6 @@
 //! All rights reserved 2022 (c) The Hash Language authors
 #![feature(generic_associated_types, box_into_inner, iter_intersperse)]
 
-use hash_alloc::Castle;
-use lazy_static::lazy_static;
-
-lazy_static! {
-    /// AST static allocator
-    pub static ref STATIC_CASTLE: Castle = Castle::new();
-}
-
 pub mod ast;
 pub mod operator;
 pub mod tree;
