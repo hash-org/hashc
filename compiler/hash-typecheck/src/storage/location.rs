e35643f127d33642f656bb722dc5e35d1b398f85
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-06-28 20:55:38 +0100

    tc: Split `LocationStore` into multiple maps.
    The `LocationStore` is now internally made up of 4 separate maps which
    all store their counterpart `LocationTarget` keys. This is done so
    that copying whole key sets is significantly cheaper in memory and
    computation since the relative key can now point to the reference
    counted value set.
    
    Another minor change is that all the functions now accept `Into<LocationTarget>`
    rather than `LocationTarget` since all callers will call `.into()`.

--- compiler/hash-typecheck/src/storage/location.rs
+++ compiler/hash-typecheck/src/storage/location.rs
@@ -3,7 +3,7 @@
 //! and declaration in one place rather than scattering them across the
 //! entire implementation of the storage.
 
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap, hash::Hash, rc::Rc};
 
 use hash_source::location::SourceLocation;
 
@@ -12,44 +12,147 @@ use super::primitives::{ArgsId, ParamsId, ScopeId, TermId};
 /// An index into the location map.
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub enum LocationTarget {
+    /// A term within stores a [TermId]
     Term(TermId),
-    Params(ParamsId, usize),
-    Args(ArgsId, usize),
+    /// A parameter key includes the parent [ParamsId] and an index to which
+    /// parameter
+    Param(ParamsId, usize),
+    /// A argument key includes the parent [ArgsId] and an index to which
+    /// argument
+    Arg(ArgsId, usize),
+    /// A declaration key includes the parent [ScopeId] and an index to which
+    /// declaration
     Declaration(ScopeId, usize),
 }
 
 /// Stores the source location of various targets in the AST tree.
 ///
 /// Not every [LocationTarget] is guaranteed to have an attached location, but
-/// if it does it will be stored here.
+/// if it does it will be stored here. The internal structure of the locations
+/// is split into 4 different maps which contain mappings between the various
+/// ids that are variants of [LocationTarget]. This is done so that the overhead
+/// of copying locations is as small as possible (since copying can occur often
+/// when ids are newly generated from either simplifications or substitutions).
+///
+/// When a set of locations (for example a [ParamsId]) is copied, the reference
+/// to the map of locations that the internal parameters refer to is copied
+/// since the inner map is behind an [Rc<T>].
+///
+/// @@Future: It would be nice to have some equivalent map but also indexed by
+/// [SourceId]. Although, this might be complicated to implement as shared data
+/// structures like [Params] might have multiple [SourceId]s.
 #[derive(Debug, Default)]
 pub struct LocationStore {
-    data: HashMap<LocationTarget, SourceLocation>,
+    /// A map between [TermId] to [SourceLocation]
+    term_map: HashMap<TermId, SourceLocation>,
+    /// A map between [ParamsId] and all of the [SourceLocation]s indexed by the
+    /// inner offset.
+    param_map: HashMap<ParamsId, Rc<RefCell<HashMap<usize, SourceLocation>>>>,
+    /// A map between [ArgsId] and all of the [SourceLocation]s indexed by the
+    /// inner offset.
+    arg_map: HashMap<ArgsId, Rc<RefCell<HashMap<usize, SourceLocation>>>>,
+    /// A map between [ScopeId] and all of the [SourceLocation]s indexed by the
+    /// inner offset.
+    declaration_map: HashMap<ScopeId, Rc<RefCell<HashMap<usize, SourceLocation>>>>,
 }
 
 impl LocationStore {
     /// Create a new [LocationStore]
     pub fn new() -> Self {
-        Self { data: HashMap::new() }
+        Self {
+            term_map: HashMap::new(),
+            param_map: HashMap::new(),
+            declaration_map: HashMap::new(),
+            arg_map: HashMap::new(),
+        }
     }
 
     /// Add a [SourceLocation] to a specified [LocationTarget]
-    pub fn add_location_to_target(&mut self, target: LocationTarget, location: SourceLocation) {
-        self.data.insert(target, location);
+    pub fn add_location_to_target(
+        &mut self,
+        target: impl Into<LocationTarget>,
+        location: SourceLocation,
+    ) {
+        match target.into() {
+            LocationTarget::Term(term) => {
+                self.term_map.insert(term, location);
+            }
+            LocationTarget::Param(param, index) => {
+                let map = self
+                    .param_map
+                    .entry(param)
+                    .or_insert_with(|| Rc::new(RefCell::new(HashMap::new())));
+                map.borrow_mut().insert(index, location);
+            }
+            LocationTarget::Arg(arg, index) => {
+                let map = self
+                    .arg_map
+                    .entry(arg)
+                    .or_insert_with(|| Rc::new(RefCell::new(HashMap::new())));
+                map.borrow_mut().insert(index, location);
+            }
+            LocationTarget::Declaration(scope, index) => {
+                let map = self
+                    .declaration_map
+                    .entry(scope)
+                    .or_insert_with(|| Rc::new(RefCell::new(HashMap::new())));
+                map.borrow_mut().insert(index, location);
+            }
+        };
     }
 
     /// Get a [SourceLocation] from a specified [LocationTarget]
-    pub fn get_location(&self, target: LocationTarget) -> Option<SourceLocation> {
-        self.data.get(&target).copied()
+    pub fn get_location(&self, target: impl Into<LocationTarget>) -> Option<SourceLocation> {
+        match target.into() {
+            LocationTarget::Term(term) => self.term_map.get(&term).copied(),
+            LocationTarget::Param(param, index) => {
+                Some(*self.param_map.get(&param)?.borrow().get(&index)?)
+            }
+            LocationTarget::Arg(arg, index) => Some(*self.arg_map.get(&arg)?.borrow().get(&index)?),
+            LocationTarget::Declaration(scope, index) => {
+                Some(*self.declaration_map.get(&scope)?.borrow().get(&index)?)
+            }
+        }
+    }
+
+    /// Copy a parameter set within [LocationStore]. This will copy the internal
+    /// reference of the src [ParamsId] to the `dest` [ParamsId]. This does not
+    /// create a new separate copy of locations.
+    pub fn copy_params_locations(&mut self, src: ParamsId, dest: ParamsId) {
+        if let Some(origin) = self.param_map.get(&src) {
+            self.param_map.insert(dest, origin.clone());
+        }
+    }
+
+    /// Copy a arguments set within [LocationStore]. This will copy the internal
+    /// reference of the src [ArgsId] to the `dest` [ArgsId]. This does not
+    /// create a new separate copy of locations.
+    pub fn copy_args_locations(&mut self, src: ArgsId, dest: ArgsId) {
+        if let Some(origin) = self.arg_map.get(&src) {
+            self.arg_map.insert(dest, origin.clone());
+        }
+    }
+
+    /// Copy a declaration set within [LocationStore]. This will copy the
+    /// internal reference of the src [ScopeId] to the `dest` [ScopeId].
+    /// This does not create a new separate copy of locations.
+    pub fn copy_declarations_locations(&mut self, src: ScopeId, dest: ScopeId) {
+        if let Some(origin) = self.declaration_map.get(&src) {
+            self.declaration_map.insert(dest, origin.clone());
+        }
     }
 
     /// Copy a location from a source [LocationTarget] to a destination target.
     ///
     /// if the `source` is not present within the store, then no location is
     /// copied.
-    pub fn copy_location(&mut self, src: LocationTarget, dest: LocationTarget) {
-        if let Some(origin) = self.get_location(src) {
-            self.add_location_to_target(dest, origin);
+    pub fn copy_location(
+        &mut self,
+        src: impl Into<LocationTarget>,
+        dest: impl Into<LocationTarget>,
+    ) {
+        if let Some(origin) = self.get_location(src.into()) {
+            self.add_location_to_target(dest.into(), origin);
         }
     }
 }
@@ -68,6 +171,12 @@ impl From<&TermId> for LocationTarget {
 
 impl From<(ParamsId, usize)> for LocationTarget {
     fn from((id, index): (ParamsId, usize)) -> Self {
-        Self::Params(id, index)
+        Self::Param(id, index)
+    }
+}
+
+impl From<(ArgsId, usize)> for LocationTarget {
+    fn from((id, index): (ArgsId, usize)) -> Self {
+        Self::Arg(id, index)
     }
 }
