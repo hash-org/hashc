c8174996da2e58b52f02cb5a3f2a9ecd6a05e803
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-07-02 13:14:12 +0100

    reporting: `panic_on_span` macro internally calls `std::panic::panic_any`

--- compiler/hash-reporting/src/macros.rs
+++ compiler/hash-reporting/src/macros.rs
@@ -1,19 +1,26 @@
 //! Utility macros for performing various operations when it comes to
 //! working with reports
 
+/// The macro `panic_on_span` is essentially used to create a [Report], build
+/// it and then print it whilst panicking. This is a useful utility to denote
+/// where a panic occurs and provide additional context about where the panic
+/// occurred in regards to traversing the sources.
 pub macro panic_on_span {
-    ($location:expr, $sources:expr, $($arg:tt)*) => {
-        let message = format_args_nl!($($arg)*);
+    ($location:expr, $sources:expr, $fmt: expr, $($arg:tt)*) => {
+        {
+            let message = format!($fmt, $($arg)*);
 
-        let mut report = $crate::builder::ReportBuilder::new();
-        report
-            .with_kind(ReportKind::Internal)
-            .with_message(message)
-            .add_element(ReportElement::CodeBlock(ReportCodeBlock::new(
-                err.location,
-                "here",
-            )));
+            let mut report = $crate::builder::ReportBuilder::new();
+            report
+                .with_kind($crate::report::ReportKind::Internal)
+                .with_message(message)
+                .add_element($crate::report::ReportElement::CodeBlock($crate::report::ReportCodeBlock::new(
+                    $location,
+                    "here",
+                )));
 
-        println!("{}", ReportWriter::new(report.build(), $sources));
+            println!("{}", $crate::writer::ReportWriter::new(report.build(), $sources));
+            std::panic::panic_any("internal compiler error");
+        }
     }
 }
