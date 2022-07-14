f52d27ba8c93ddd523963aff5d39b82d3159851f
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-07-14 15:57:19 -0400

    parser: parse `~` as `BinOp::Merge` within parse_binary_operator

--- compiler/hash-parser/src/parser/operator.rs
+++ compiler/hash-parser/src/parser/operator.rs
@@ -24,6 +24,10 @@ impl<'stream, 'resolver> AstGen<'stream, 'resolver> {
         match &(token.unwrap()).kind {
             // Since the 'as' keyword is also a binary operator, we have to handle it here...
             TokenKind::Keyword(Keyword::As) => (Some(BinOp::As), 1),
+            TokenKind::Tilde => match self.peek_second() {
+                Some(token) if token.kind == TokenKind::Eq =>(None, 0), // merge declaration
+                _ =>  (Some(BinOp::Merge), 1),
+            }
             TokenKind::Eq => match self.peek_second() {
                 Some(token) if token.kind == TokenKind::Eq => (Some(BinOp::EqEq), 2),
                 _ => (None, 0),
