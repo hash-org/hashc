4a379b0b3d5646eff41397214edd6b3538efab3b
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-07-20 12:07:34 -0400

    utils: fix panic on non-existent path

--- compiler/hash-utils/src/path.rs
+++ compiler/hash-utils/src/path.rs
@@ -2,15 +2,18 @@
 
 use std::{fs::canonicalize, path::Path};
 
+/// Function to apply formatting onto a path when printing it.
 #[cfg(not(target_os = "windows"))]
 pub fn adjust_canonicalization<P: AsRef<Path>>(p: P) -> String {
-    canonicalize(p).unwrap().display().to_string()
+    canonicalize(p.as_ref()).unwrap_or_else(|_| p.as_ref().to_path_buf()).display().to_string()
 }
 
+/// Function to apply formatting onto a path when printing it.
 #[cfg(target_os = "windows")]
 pub fn adjust_canonicalization<P: AsRef<Path>>(p: P) -> String {
     const VERBATIM_PREFIX: &str = r#"\\?\"#;
-    let p = canonicalize(p).unwrap().display().to_string();
+    let p =
+        canonicalize(p.as_ref()).unwrap_or_else(|_| p.as_ref().to_path_buf()).display().to_string();
 
     if p.starts_with(VERBATIM_PREFIX) {
         p[VERBATIM_PREFIX.len()..].to_string()
