8287399d039a36aa57b4abb0a6f63b9ff157b65c
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-07-28 14:38:03 -0400

    source: add `DUMMY_SPAN` constant to represent a default `Span`

--- compiler/hash-source/src/location.rs
+++ compiler/hash-source/src/location.rs
@@ -3,12 +3,16 @@ use crate::SourceId;
 use derive_more::Constructor;
 use std::{convert::TryInto, fmt};
 
+/// This is used as a filler span when generating constructors that
+/// fill in patterns.
+pub const DUMMY_SPAN: Span = Span(0, 0);
+
 /// Enum representing a location of a token within the source.
 ///
 /// The first element of the tuple represents the starting byte offset and the
 /// second element represents the ending byte offset.
 #[derive(Debug, Eq, Hash, Clone, Copy, PartialEq)]
-pub struct Span(u32, u32);
+pub struct Span(pub u32, pub u32);
 
 /// General functions to create [Span] variants
 impl Span {
@@ -16,7 +20,7 @@ impl Span {
     pub fn new(start: usize, end: usize) -> Self {
         debug_assert!(
             end >= start,
-            "Got invalid span for Location::span. Start needs to be smaller than end."
+            "Got invalid span for Span::new. Start needs to be smaller than end."
         );
 
         Span(start.try_into().unwrap(), end.try_into().unwrap())
