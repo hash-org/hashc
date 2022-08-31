ea024a2db921e3649e4d0837034d192f9e7c4aeb
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-08-31 14:14:24 -0400

    compiler: add new `hash_types` crate for sharing type representation amongst dependant crates

---
+++ compiler/hash-types/src/lib.rs
@@ -0,0 +1,4 @@
+//! Crate that contains all of the mechanisms that are used for storing
+//! information about types within the Hash type system. This crate is
+//! primarily used by `hash_typecheck` and `hash_lower` in order to
+//! share the type representation.
