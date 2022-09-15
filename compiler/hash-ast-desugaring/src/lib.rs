368f8efa31cb1ae5fb37f72bea193b03b8b2339d
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-09-15 18:28:38 +0100

    fix: remove `generic_associated_types` feature flag due to recent stabilisation

--- compiler/hash-ast-desugaring/src/lib.rs
+++ compiler/hash-ast-desugaring/src/lib.rs
@@ -2,7 +2,6 @@
 //! visitor pattern on the AST in order to `lower` it to a simpler version so
 //! that later stages can work on it without having to operate on similar
 //! constructs and duplicating logic.
-#![feature(generic_associated_types)]
 
 use std::collections::HashSet;
 
