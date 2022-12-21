15cdafca9214db78576cbf2953cbd6b5ea449a44
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-12-21 15:40:40 +0000

    ir: begin sketching out an IR visitor trait family

--- compiler/hash-ir/src/ir.rs
+++ compiler/hash-ir/src/ir.rs
@@ -10,7 +10,7 @@ use hash_source::{
     location::{SourceLocation, Span},
     SourceId,
 };
-use hash_types::{scope::ScopeId, terms::TermId};
+use hash_types::scope::ScopeId;
 use hash_utils::{
     new_sequence_store_key, new_store_key,
     store::{DefaultSequenceStore, DefaultStore, SequenceStore, Store},
@@ -24,6 +24,8 @@ use crate::{
     IrStorage,
 };
 
+/// A specified constant value within the Hash IR. These values and their
+/// shape is always known at compile-time.
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
 pub enum Const {
     /// Nothing, it has zero size, and is associated with a particular type.
@@ -471,7 +473,7 @@ pub enum RValue {
     /// @@Future: maybe in the future this should be replaced by a compile-time
     /// API variant which will just run some kind of operation and return the
     /// constant.
-    ConstOp(ConstOp, TermId),
+    ConstOp(ConstOp, IrTyId),
 
     /// A unary expression with a unary operator.
     UnaryOp(UnaryOp, RValueId),
@@ -809,7 +811,8 @@ impl TerminatorKind {
 /// terminator. Initially, the `terminator` begins as [None], and will
 /// be set when the lowering process is completed.
 ///
-/// N.B. It is an invariant for a [BasicBlock] to not have a terminator.
+/// N.B. It is an invariant for a [BasicBlock] to not have a terminator
+/// once it has been built.
 #[derive(Debug, PartialEq, Eq)]
 pub struct BasicBlockData {
     /// The statements that the block has.
--- compiler/hash-ir/src/visitor.rs
+++ compiler/hash-ir/src/visitor.rs
@@ -1,24 +1,140 @@
-//! Hash Compiler Intermediate Representation (IR) crate.
-
-use std::process::Output;
+//! Hash IR visitor and walker trait definitions.
 
 use crate::{
-    ir::{BasicBlock, Body, RValue, Statement, Terminator},
-    IrStorage,
+    ir::{
+        AddressMode, AggregateKind, AssertKind, BasicBlock, BasicBlockData, BinOp, Body, ConstKind,
+        ConstOp, Local, Place, RValue, RValueId, Statement, SwitchTargets, Terminator,
+    },
+    ty::{IrTyId, Mutability, VariantIdx},
+    BodyDataStore,
 };
 
+/// A trait for visiting the IR with a mutable context. This trait should
+/// not be used for when modifications to the IR need to be made in place.
+/// This is because the IR values are mapped from the [RValueStore] and
+/// other stores in such a way that prevents in place modification. Any
+/// modifications should be made by modifying the [RValue]s after the process
+/// of visiting is complete or use the [ModifyingIrVisitor] trait families.
+pub trait IrVisitorMut<'ir> {
+    /// Return a reference to the [BodyDataStore].
+    fn storage(&self) -> &'ir BodyDataStore;
+
+    /// Return the [Body] of the current visitor.
+    fn body(&self) -> &'ir Body;
+
+    type BodyRet;
+    fn visit_body(&mut self, body: &'ir Body) -> Self::BodyRet;
+
+    type BlockRet;
+    fn visit_block(&mut self, block: &'ir BasicBlockData) -> Self::BlockRet;
+
+    type StatementRet;
+    fn visit_statement(&mut self, statement: &'ir Statement) -> Self::StatementRet;
+
+    fn visit_nop_statement(&mut self) -> Self::StatementRet;
+
+    fn visit_assign_statement(&mut self, place: &'ir Place, value: RValueId) -> Self::StatementRet;
+
+    fn visit_discriminator_statement(
+        &mut self,
+        place: &'ir Place,
+        variant: VariantIdx,
+    ) -> Self::StatementRet;
+
+    fn visit_alloc_statement(&mut self, local: Local) -> Self::StatementRet;
+
+    fn visit_alloc_raw_statement(&mut self, local: Local) -> Self::StatementRet;
+
+    type RValueRet;
+    fn visit_rvalue(&mut self, value: &'ir RValue) -> Self::RValueRet;
+
+    fn visit_const_rvalue(&mut self, value: &'ir ConstKind) -> Self::RValueRet;
+
+    fn visit_use_rvalue(&mut self, value: &'ir Place) -> Self::RValueRet;
+
+    fn visit_const_op_rvalue(&mut self, op: ConstOp, ty: IrTyId) -> Self::RValueRet;
+
+    fn visit_checked_binary_op_rvalue(
+        &mut self,
+        op: BinOp,
+        lhs: RValueId,
+        rhs: RValueId,
+    ) -> Self::RValueRet;
+
+    fn visit_len_rvalue(&mut self, value: &'ir Place) -> Self::RValueRet;
+
+    fn visit_ref_rvalue(
+        &mut self,
+        mutability: Mutability,
+        value: &'ir Place,
+        mode: AddressMode,
+    ) -> Self::RValueRet;
+
+    fn visit_aggregate_rvalue(
+        &mut self,
+        kind: AggregateKind,
+        values: &'ir [RValueId],
+    ) -> Self::RValueRet;
+
+    fn visit_discriminant(&mut self, value: &'ir Place) -> Self::RValueRet;
+
+    type TerminatorRet;
+    fn visit_terminator(&mut self, terminator: &'ir Terminator) -> Self::TerminatorRet;
+
+    fn visit_goto_terminator(&mut self, target: BasicBlock) -> Self::TerminatorRet;
+
+    fn visit_unreachable_terminator(&mut self) -> Self::TerminatorRet;
+
+    fn visit_return_terminator(&mut self) -> Self::TerminatorRet;
+
+    fn visit_call_terminator(
+        &mut self,
+        op: RValueId,
+        args: &'ir [RValueId],
+        destination: &'ir Place,
+        target: Option<BasicBlock>,
+    ) -> Self::TerminatorRet;
+
+    fn visit_switch_terminator(
+        &mut self,
+        value: RValueId,
+        targets: &'ir SwitchTargets,
+    ) -> Self::TerminatorRet;
+
+    fn visit_assert_terminator(
+        &mut self,
+        condition: &'ir Place,
+        expected: bool,
+        kind: AssertKind,
+        target: BasicBlock,
+    ) -> Self::TerminatorRet;
+
+    type PlaceRet;
+    fn visit_place(&mut self, place: &'ir Place) -> Self::PlaceRet;
+}
+
+// @@Todo: fill these in when the general structure of `IrVisitorMut` is
+// finalised.
 pub trait IrVisitor<'ir> {
-    type Output;
+    /// Return a reference to the [BodyDataStore].
+    fn storage(&self) -> &'ir BodyDataStore;
 
-    fn storage(&self) -> &'ir IrStorage;
+    /// Return the [Body] of the current visitor.
+    fn body(&self) -> &'ir Body;
+}
 
-    fn visit_body(&mut self, body: &'ir Body) -> Output;
+pub trait ModifyingIrVisitor<'ir> {
+    /// Return a reference to the [BodyDataStore].
+    fn storage(&self) -> &'ir BodyDataStore;
 
-    fn visit_block(&mut self, block: BasicBlock) -> Output;
+    /// Return the [Body] of the current visitor.
+    fn body(&self) -> &'ir Body;
+}
 
-    fn visit_statement(&mut self, statement: &'ir Statement) -> Output;
+pub trait ModifyingIrVisitorMut<'ir> {
+    /// Return a reference to the [BodyDataStore].
+    fn storage(&self) -> &'ir BodyDataStore;
 
-    fn visit_rvalue(&mut self, value: &'ir RValue) -> Output;
-
-    fn visit_terminator(&mut self, terminator: &'ir Terminator) -> Output;
+    /// Return the [Body] of the current visitor.
+    fn body(&self) -> &'ir Body;
 }
