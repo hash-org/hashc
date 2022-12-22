8b73df8bfae636f0fbe023710a45cd445892ba02
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-12-22 13:42:09 +0000

    codegen: add crates for code generation

---
+++ compiler/hash-codegen-llvm/src/lib.rs
@@ -0,0 +1,3 @@
+//! Hash compiler LLVM code generation crate. This crate contains all of the
+//! logic of transforming generated Hash IR into LLVM IR so that it can be
+//! compiled by LLVM into a native executable with the specified target triple.
---
+++ compiler/hash-codegen/src/lib.rs
@@ -0,0 +1,19 @@
+//! Hash compiler code generation crate. This crate contains interfaces for 
+//! a code generation stage to implement a translation stage between Hash IR
+//! into some code representation for a backend to process. The current available
+//! backends are the following:
+//! 
+//! 1. The LLVM backend which translates Hash IR into LLVM IR so that it can be
+//!    compiled by LLVM into a native executable with the specified target triple.
+//!    The LLVM backend is located in the `hash-codegen-llvm` crate.
+//! 
+//! 2. The bytecode backend which translates Hash IR into Hash Bytecode so that it
+//!   can be processed by the Hash VM. The bytecode backend is located in the
+//!  `hash-codegen-bytecode` crate.
+//! 
+//! A backend that implements the `CodeGen` trait can be used to generate code 
+//! for a specific target, and then the compiler plumbing will continue after 
+//! the code is generated. Different backends may produce different "artifacts"
+//! based on their requirements. For example, the VM backend will not emit any 
+//! artifacts since it will run the bytecode directly using the VM. On the other 
+//! hand, LLVM backend will emit a runnable executable after the code is generated.
