eef29d9a853eea52020bd8d7a76ce63b714bcccd
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-12-22 13:55:32 +0000

    all: cleanup and standardise `Cargo.toml` formats

--- compiler/hash-codegen-bytecode/src/lib.rs
+++ compiler/hash-codegen-bytecode/src/lib.rs
@@ -1,3 +1,3 @@
-//! Hash compiler bytecode code generation crate. This crate contains 
+//! Hash compiler bytecode code generation crate. This crate contains
 //! all of the logic of transforming generated Hash IR into Hash Bytecode
 //! so that it can be processed by the Hash VM.
