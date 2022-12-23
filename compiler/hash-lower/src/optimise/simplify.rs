db914cb5ec07cbf9e6c52a323cbf2ba21e99032b
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-12-23 01:01:13 +0000

    lower: massively improve performance of graph simplification pass.
    Previously, this pass would continuously thrash the `predecessors`
    cache on the CFG when updating basic blocks. This meant that
    every time a block was updated, the whole graph predecessors
    map would be re-computed. This means that it would be re-computed
    per update, per block elimination, per pass. This is obviously very bad.
    
    Now, the pass only computes predecessors once, and keeps an up-to-date
    record as it eliminates dead blocks. Additionally, the pass now
    tries to eliminate `goto` chains as it goes, which means it can
    eliminate a lot more blocks in one pass, and thus needs to perform
    fewer passes to scrub the entire graph.
    
    We went down from a `550ms` to a measly `1ms`!

--- compiler/hash-lower/src/optimise/simplify.rs
+++ compiler/hash-lower/src/optimise/simplify.rs
@@ -12,10 +12,12 @@
 //!   have been updated, the current block is removed from the body.
 
 use hash_ir::{
-    ir::{BasicBlock, Body, TerminatorKind},
+    ir::{BasicBlock, BasicBlockData, Body, Terminator, TerminatorKind, START_BLOCK},
     BodyDataStore,
 };
 use hash_pipeline::settings::{LoweringSettings, OptimisationLevel};
+use index_vec::{index_vec, Idx, IndexVec};
+use smallvec::{smallvec, SmallVec};
 
 use super::IrOptimisation;
 use crate::traversal;
@@ -56,10 +58,37 @@ pub fn remove_dead_blocks(body: &mut Body) {
     // of the block terminators with the terminators that are
     // specified within the replacement map.
     for block in basic_blocks {
-        for target in block.terminator_mut().successors() {
-            block.terminator_mut().replace_edge(target, replacement_map[target.index()])
+        for target in block.terminator_mut().successors_mut() {
+            *target = replacement_map[target.index()];
         }
     }
+}
+
+fn copy_index_vec<S, T: Clone, I: Idx>(value: T, previous: &IndexVec<I, S>) -> IndexVec<I, T> {
+    index_vec![value; previous.len()]
+}
+
+/// This function is used to compute a predecessor count for each block, and
+/// returns a vector of the counts. This is used to determine if a block has
+/// a single predecessor, and therefore can be removed.
+fn compute_predecessor_counts(body: &Body) -> IndexVec<BasicBlock, u32> {
+    let mut counts = copy_index_vec(0, &body.basic_blocks.blocks);
+
+    // We have to always set the count of the basic block as `1` since
+    // it might not ever have any predecessors, but it is still reachable.
+    counts[START_BLOCK] = 1;
+
+    let traverser = traversal::PreOrder::new_from_start(body);
+
+    for (_, data) in traverser {
+        if let Some(ref term) = data.terminator {
+            for target in term.successors() {
+                counts[target] += 1;
+            }
+        }
+    }
+
+    counts
 }
 
 pub struct SimplifyGraph;
@@ -74,13 +103,30 @@ impl IrOptimisation for SimplifyGraph {
     }
 
     fn optimise(&self, body: &mut Body, _: &BodyDataStore) {
-        // Firstly, lets check if there are any predecessors for each block. If the
-        // block has no predecessors then we remove the block from the body entirely.
-        // This is because the block is unreachable.
-        //
-        // It's safe not to invalidate the cache here since we are removing dead blocks.
-        remove_dead_blocks(body);
+        GraphSimplifier::new(body).simplify();
 
+        // Now we can remove the blocks that we no longer need.
+        remove_dead_blocks(body);
+    }
+}
+
+pub struct GraphSimplifier<'ir> {
+    /// A reference to all of the basic blocks in the graph
+    basic_blocks: &'ir mut IndexVec<BasicBlock, BasicBlockData>,
+
+    /// A reference to the predecessor count for each block
+    predecessor_count: IndexVec<BasicBlock, u32>,
+}
+
+impl<'ir> GraphSimplifier<'ir> {
+    /// Create a new [`GraphSimplifier`] from the given body
+    pub fn new(body: &'ir mut Body) -> Self {
+        let predecessor_count = compute_predecessor_counts(body);
+
+        GraphSimplifier { basic_blocks: body.basic_blocks.blocks_mut(), predecessor_count }
+    }
+
+    pub fn simplify(mut self) {
         // Now we try to simplify the graph by removing blocks that only perform
         // a goto to another block. We perform this optimisation until we reached
         // a fixed point since it maybe be that we can remove multiple blocks in
@@ -88,48 +134,96 @@ impl IrOptimisation for SimplifyGraph {
         loop {
             let mut changed = false;
 
-            // stores the index of the block that can be removed, and the block
-            // that it should be substituted with.
-            let mut changes: Vec<(BasicBlock, BasicBlock)> = Vec::new();
-
-            for (index, block) in body.blocks().iter_enumerated() {
-                // If there are any statements in the block, or if the block is the
-                // starting block, we cannot remove it since the entry point is
-                // always the starting block.
-                if !block.statements.is_empty()
-                    || body.basic_blocks.predecessors_of(index).is_empty()
-                {
+            for index in self.basic_blocks.indices() {
+                // if this block has no predecessors, then we can just skip it
+                if self.predecessor_count[index] == 0 {
                     continue;
                 }
 
-                if let TerminatorKind::Goto(substitution) = block.terminator.as_ref().unwrap().kind
-                {
-                    // we want to get all of the predecessors of this block, and substitute any
-                    // references to the current block with the destination block.
-                    changes.push((index, substitution));
-                    changed = true;
+                let mut terminator = self.basic_blocks[index].terminator.take().unwrap();
+
+                // attempt to simplify the goto chain
+                for successor in terminator.successors_mut() {
+                    self.fold_goto_chain(successor, &mut changed);
                 }
+
+                // @@Future: we could try to perform some optimisations with
+                // merging blocks, and some branch simplification.
+
+                // Reset the terminator of the block
+                self.basic_blocks[index].terminator = Some(terminator);
             }
 
             // Short-circuit if we didn't make any changes.
             if !changed {
                 break;
             }
+        }
+    }
 
-            // Now we can perform the changes to the body.
-            for (index, substitution) in changes {
-                let predecessors = body.basic_blocks.predecessors_of(index);
+    /// Traverse along a line of of goto chains and replace them with a single
+    /// jump. These jump chains can occur in common with complex `match`
+    /// expressions or `loop` control flow.
+    fn fold_goto_chain(&mut self, block: &mut BasicBlock, changed: &mut bool) {
+        // @@Note: usually the go-to chain will be of length 1, so we use a
+        // smallvec here.
+        let mut terminators: SmallVec<[_; 1]> = smallvec![];
 
-                for predecessor in predecessors {
-                    if let Some(block) = body.basic_blocks.blocks_mut().get_mut(predecessor) {
-                        let terminator = block.terminator.as_mut().unwrap();
-                        terminator.replace_edge(index, substitution);
-                    }
-                }
+        // Begin by traversing up the successor chain until we reach a
+        // block that does not have a terminator which is a goto.
+        let mut current = *block;
+
+        while let Some(terminator) = self.take_terminator_on_goto(current) {
+            let Terminator { kind: TerminatorKind::Goto(target), .. } = terminator else {
+                unreachable!()
+            };
+
+            terminators.push((current, terminator));
+            current = target;
+        }
+
+        let last = current;
+        *block = last;
+
+        // Now we iterate over all of the blocks that we have found to
+        // be in the chain and update our `predecessor_count` to reflect
+        // their removal.
+        while let Some((current, mut terminator)) = terminators.pop() {
+            let Terminator { kind: TerminatorKind::Goto (ref mut target), .. } = terminator else {
+                unreachable!();
+            };
+
+            // update changed, and update the target
+            *changed |= *target != last;
+            *target = last;
+
+            // If this is the last predecessor, then we don;t need to update
+            // the count of the `target` since it will be removed.
+            if self.predecessor_count[current] == 1 {
+                self.predecessor_count[current] = 0;
+            } else {
+                self.predecessor_count[*target] += 1;
+                self.predecessor_count[current] -= 1;
             }
 
-            // Now we can remove the blocks that we no longer need.
-            remove_dead_blocks(body);
+            // finally, update the terminator of the current block
+            self.basic_blocks[current].terminator = Some(terminator);
+        }
+    }
+
+    /// Take the terminator from the given block, if it is a
+    /// [TerminatorKind::Goto]. Additionally, the block must not have any
+    /// statements since this may is not trivially collapsable.
+    ///
+    /// This is used continue traversing a goto chain.
+    fn take_terminator_on_goto(&mut self, block: BasicBlock) -> Option<Terminator> {
+        match self.basic_blocks[block] {
+            BasicBlockData {
+                ref statements,
+                terminator:
+                    ref mut terminator @ Some(Terminator { kind: TerminatorKind::Goto(_), .. }),
+            } if statements.is_empty() => terminator.take(),
+            _ => None,
         }
     }
 }
