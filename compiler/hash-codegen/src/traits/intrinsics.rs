d135bf349cf748930bf204e91bd7887bd0bb5525
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-12-29 17:09:37 +0000

    codegen: add constant value generation methods trait

--- compiler/hash-codegen/src/traits/intrinsics.rs
+++ compiler/hash-codegen/src/traits/intrinsics.rs
@@ -1,16 +1,23 @@
-//! This defines all of the traits to generate **intrinsic** specific 
-//! code for a target backend. The intrinsics for Hash are defined in 
+//! This defines all of the traits to generate **intrinsic** specific
+//! code for a target backend. The intrinsics for Hash are defined in
 //! the `prelude` module within the standard library, and are used to
 //! provide a common interface for the standard library to interact with
 //! compiler.
 
+use hash_ir::ty::IrTyId;
 
+use super::BackendTypes;
+
+/// Trait that is used to generate intrinsic calls for a target backend.
+///
+/// @@Todo: this is probably where `va_*` intrinsic functions would be
+/// defined.
 pub trait BuildIntrinsicCallMethods<'b>: BackendTypes {
+    /// Generate a call to an intrinsic function.
     fn codegen_intrinsic_call(
         &self,
-        intrinsic: Intrinsic,
+        ty: IrTyId,
         args: &[Self::Value],
         result: Self::Value,
     ) -> Self::Value;
-} 
-
+}
