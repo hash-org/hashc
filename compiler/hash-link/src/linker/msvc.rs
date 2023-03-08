23c997f7a3d6400a623da0da1ceb75716d5f95c3
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2023-03-08 13:49:23 +0000

    link: implement `Mold` and `Msvc` linker variants

--- compiler/hash-link/src/linker/msvc.rs
+++ compiler/hash-link/src/linker/msvc.rs
@@ -1,13 +1,11 @@
 //! Contains all of the logic and utilities that surround the
 //! MSVC linker.
-#![allow(unused)]
 
-use std::path::Path;
+use std::{ffi::OsString, path::Path};
 
-use cc::Tool;
-use hash_pipeline::settings::CompilerSettings;
+use hash_pipeline::settings::{CompilerSettings, OptimisationLevel};
 
-use super::Linker;
+use super::{LinkOutputKind, Linker};
 use crate::command::LinkCommand;
 
 pub struct MsvcLinker<'ctx> {
@@ -23,38 +21,61 @@ pub struct MsvcLinker<'ctx> {
 
 impl<'ctx> Linker for MsvcLinker<'ctx> {
     fn cmd(&mut self) -> &mut LinkCommand {
-        todo!()
+        &mut self.command
     }
 
-    fn set_output_kind(&mut self, kind: super::LinkOutputKind) {
-        todo!()
+    fn set_output_kind(&mut self, _: LinkOutputKind) {
+        // We don't have to anything for the MSVC linker, @@Future: however
+        // when we add dynamic/static library building, we need to
+        // deal with specifying the filename here.
     }
 
     fn set_output_filename(&mut self, filename: &Path) {
-        todo!()
+        let mut arg = OsString::from("/OUT:");
+        arg.push(filename);
+        self.command.arg(&arg);
     }
 
-    fn link_dylib(&mut self, lib: &str, verbatim: bool, as_needed: bool) {
-        todo!()
+    fn link_dylib(&mut self, lib: &str, verbatim: bool, _: bool) {
+        self.command.arg(format!("{}{}", lib, if verbatim { "" } else { ".lib" }));
     }
 
     fn link_static_lib(&mut self, lib: &str, verbatim: bool) {
-        todo!()
+        self.command.arg(format!("{}{}", lib, if verbatim { "" } else { ".lib" }));
     }
 
     fn include_path(&mut self, path: &Path) {
-        todo!()
+        let mut arg = OsString::from("/LIBPATH:");
+        arg.push(path);
+        self.command.arg(&arg);
     }
 
     fn add_object(&mut self, path: &Path) {
-        todo!()
+        self.command.arg(path);
     }
 
+    /// For MSVC, the linker will by default perform the following:
+    /// > When used at the command line, the linker defaults to
+    /// > /OPT:REF,ICF,LBR. If /DEBUG is specified, the default is
+    /// > /OPT:NOREF,NOICF,NOLBR.
+    ///
+    /// Reference: <https://learn.microsoft.com/en-us/cpp/build/reference/opt-optimizations?view=msvc-170#remarks>
     fn optimize(&mut self) {
-        todo!()
+        // No-op
     }
 
+    /// For MSVC, specifying that the linker should remove un-used items can
+    /// be done via `/OPT:REF` and `/OPT:ICF`. The former removes un-used
+    /// functions and data, while the latter removes duplicate functions
+    /// and data.
+    ///
+    /// Documentation for the `/OPT` flag can be found here:
+    /// <https://learn.microsoft.com/en-us/cpp/build/reference/opt-optimizations?view=msvc-170>
     fn gc_sections(&mut self) {
-        todo!()
+        if self.settings.optimisation_level == OptimisationLevel::Debug {
+            self.command.arg("/OPT:REF,ICF");
+        } else {
+            self.command.arg("/OPT:REF,NOICF");
+        }
     }
 }
