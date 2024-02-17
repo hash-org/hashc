752a2342f7da632a76728e8f28fa5b9ad718ebf5
Alexander Fedotov <alexfedotov@myurbanjungle.com>
2024-02-17 22:55:47 +0000

    ast: remove un-used `ty` module

--- compiler/hash-ast/src/ty.rs
+++
@@ -1,5 +0,0 @@
-//! Primitive type kinds at the AST level, this is only really
-//! used for either pretty printing types, or for interfacing with
-//! the compiler [Constantt] system.
-
-use hash_source::constant::{FloatTy, IntTy, SIntTy, Ty, UIntTy};
