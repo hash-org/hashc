ba4611569800eddcf36f77da7aacd8c164c7e201
Alexander Fedotov <alexfedotov@myurbanjungle.com>
2024-04-07 20:49:12 +0100

    wip

--- compiler/hash-messaging/src/lib.rs
+++ compiler/hash-messaging/src/lib.rs
@@ -1,1 +1,33 @@
 //! Defines compiler messages that are passed in and out of the compiler.
+
+pub mod listener;
+pub mod stream;
+
+use hash_reporting::report::Report;
+
+/// The [CompilerMessagingFormat] specifies the message mode that the compiler
+/// will use to to emit and receive messages.
+#[derive(Debug, Clone)]
+pub enum CompilerMessagingFormat {
+    /// All messages that are emitted to and from the compiler will be in JSON
+    /// format according to the schema that represents [CompilerMessage].
+    Json,
+
+    /// Normal mode is the classic emission of messages as the compiler would
+    /// normally do, i.e. calling [fmt::Display] on provided [Report]s.
+    ///
+    /// @@Note: do we support listening to messages in this mode - this doesn't
+    /// really make sense?
+    Normal,
+}
+
+#[derive(Debug, Clone)]
+pub enum CompilerOutputMessage {
+    /// A message that is emitted by the compiler, this is any diagnostic.
+    Report(Report),
+}
+
+#[derive(Debug, Clone)]
+pub enum CompilerInputMessage {
+    
+}
---
+++ compiler/hash-messaging/src/listener.rs
@@ -0,0 +1,3 @@
+//! Defines a listener interface that can accept messages into the compiler.
+//! This is listened by the compiler and the compiler can consequently emit
+//! messages as a response to the sender.
