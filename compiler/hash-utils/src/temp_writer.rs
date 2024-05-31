a6ab959b0f3084aa7a4602ba64c0ff89bc5c8fda
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-05-31 21:46:42 +0100

    utils(temp_writer): make it printable by using `Display`

--- compiler/hash-utils/src/temp_writer.rs
+++ compiler/hash-utils/src/temp_writer.rs
@@ -1,4 +1,5 @@
 //! Utility for temporarily writing something to a buffer.
+use core::fmt;
 use std::io;
 
 use derive_more::Constructor;
@@ -8,7 +9,11 @@ pub struct TempWriter(Vec<u8>);
 
 impl TempWriter {
     pub fn into_string(self) -> String {
-        String::from_utf8(self.0).unwrap()
+        unsafe { String::from_utf8_unchecked(self.0) }
+    }
+
+    pub fn as_str(&self) -> &str {
+        unsafe { std::str::from_utf8_unchecked(&self.0) }
     }
 }
 
@@ -20,5 +25,11 @@ impl io::Write for TempWriter {
 
     fn flush(&mut self) -> io::Result<()> {
         Ok(())
+    }
+}
+
+impl fmt::Display for TempWriter {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_str())
     }
 }
